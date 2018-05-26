14223c46e2c23c84fd9ffe0b0ed070e1ccda5882
zamp <xythec@gmail.com>
2018-05-27 00:32:41 +0300

    Added glove box snapping

--- TrunkSnap/TrunkSnap.cs
+++ TrunkSnap/TrunkSnap.cs
@@ -6,7 +6,7 @@ namespace TrunkSnap
 	public class TrunkSnap : Mod
 	{
 		public override string ID => "TrunkSnap";
-		public override string Version => "0.1";
+		public override string Version => "0.2";
 		public override string Author => "zamp";
 
 		public override void OnLoad()
@@ -30,17 +30,24 @@ namespace TrunkSnap
 
 			trigger.GetComponent<Collider>().isTrigger = true;
 			trigger.AddComponent<TrunkSnapBehaviour>();
-		}
 
-		public override void Update()
-		{
-			return;
-			if (Input.GetKey(KeyCode.Z))
-			{
-				var satsuma = PlayMakerGlobals.Instance.Variables.GetFsmGameObject("TheCar").Value;
-				satsuma.GetComponent<Rigidbody>().AddForce(Vector3.up * Random.Range(-200f,200f), ForceMode.Impulse);
-				satsuma.GetComponent<Rigidbody>().AddForce(Vector3.left * Random.Range(-200f,200f), ForceMode.Impulse);
-			}
+
+			trigger = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			trigger.transform.SetParent(satsuma.transform, false);
+			trigger.name = "GlobeBoxTrigger";
+
+			Object.Destroy(trigger.GetComponent<Rigidbody>());
+#if DEBUG
+			trigger.GetComponent<MeshRenderer>().material.color = new Color(1f, 0, 0, 0.2f);
+#else
+			Object.Destroy(trigger.GetComponent<MeshRenderer>());
+			Object.Destroy(trigger.GetComponent<MeshFilter>());
+#endif
+			trigger.transform.localPosition = new Vector3(0.321f, 0.3f, 0.62f);
+			trigger.transform.localScale = new Vector3(0.3f, 0.15f, 0.1f);
+
+			trigger.GetComponent<Collider>().isTrigger = true;
+			trigger.AddComponent<TrunkSnapBehaviour>();
 		}
 	}
 }
